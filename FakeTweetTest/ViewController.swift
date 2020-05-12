df0726e1ddb0228a5018bddf2ac32f24973be64f
Abinodh Thomas <abinodh@steamclock.com>
2020-05-12 15:00:18 -0700

    Initial Commit

---
+++ FakeTweetTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FakeTweetTest
+//
+//  Created by Abinodh Thomas on 2020-05-12.
+//  Copyright © 2020 Abinodh Thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
